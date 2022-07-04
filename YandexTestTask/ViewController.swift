0da76ef744c6a57495593e0d653f86bd6ab019ee
Filin <nastia-dogf@mail.ru>
2022-07-04 15:04:16 +0300

    added most of thing on the screen

--- YandexTestTask/ViewController.swift
+++ YandexTestTask/ViewController.swift
@@ -8,12 +8,90 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+// MARK: Outlets
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var gameFieldView: UIView!
+    @IBOutlet weak var lastScoreLabel: UILabel!
+    @IBOutlet weak var timeStepper: UIStepper!
+    @IBOutlet weak var startStopButton: UIButton!
+    @IBOutlet weak var shapeX: NSLayoutConstraint!
+    @IBOutlet weak var shapeY: NSLayoutConstraint!
+    
+// MARK: Varialble
+    var gameTimer: Timer = Timer()
+    var shapeTimer = Timer()
+    var timerIsActive = false
+    var countOfTimer = 30 {
+        didSet {
+            changedTimeLabel()
+        }
+    }
+    let displayDuration: TimeInterval = 2
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //MARK: field
+        gameFieldView.layer.borderColor = UIColor.gray.cgColor
+        gameFieldView.layer.borderWidth = CGFloat(1.0)
+        gameFieldView.layer.cornerRadius = CGFloat(8.0)
     }
 
-
+    @IBAction func stepperChanged(_ sender: UIStepper) {
+        countOfTimer = Int(sender.value)
+    }
+    
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        if !timerIsActive {
+            startGame()
+        } else {
+            stopGame()
+        }
+        
+    }
+    
+    @objc func tick() {
+        countOfTimer -= 1
+        if countOfTimer == 0 {
+            gameTimer.invalidate()
+        }
+    }
+    
+    func startGame() {
+        timeStepper.isEnabled = false
+        timerIsActive = true
+        changedTimeLabel()
+        gameTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(tick), userInfo: nil, repeats: true)
+        shapeTimer = Timer.scheduledTimer(timeInterval: displayDuration, target: self, selector: #selector(moveImage), userInfo: nil, repeats: true)
+        startStopButton.setTitle("Остановить", for: .normal)
+    }
+    
+    
+    func stopGame() {
+        gameTimer.invalidate()
+        shapeTimer.invalidate()
+        timerIsActive = false
+        countOfTimer = 30
+        timeStepper.isEnabled = true
+        startStopButton.setTitle("Начать", for: .normal)
+        lastScoreLabel.text = "Последний счет: 1"
+    }
+    
+    func changedTimeLabel() {
+        if timerIsActive {
+            mainLabel.text = "Осталось: \(countOfTimer) сек"
+        } else {
+            mainLabel.text = "Время: \(countOfTimer) сек"
+        }
+    }
+    
+    @objc func moveImage() {
+        let maxX = gameFieldView.bounds.maxX
+        let maxY = gameFieldView.bounds.maxY
+        shapeX.constant = CGFloat(arc4random_uniform(UInt32(maxX)))
+        shapeY.constant = CGFloat(arc4random_uniform(UInt32(maxY)))
+    }
+    
 }
-
