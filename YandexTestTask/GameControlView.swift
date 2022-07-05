d0c75b317d6b0fabe9b13f0bbbf7a68dda88113c
Filin <nastia-dogf@mail.ru>
2022-07-05 13:43:30 +0300

    Connect xib and class
    added xib owner 
    added method for view

--- YandexTestTask/GameControlView.swift
+++ YandexTestTask/GameControlView.swift
@@ -5,4 +5,14 @@
 //  Created by Anastasia Perminova on 05.07.2022.
 //
 
-import Foundation
+import UIKit
+
+@IBDesignable
+class GameControlView: UIView {
+    
+    private func loadViewFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: "GameControlView", bundle: bundle)
+        return nib.instantiate(withOwner: self, options: nil).first! as! UIView
+    }
+}
